26c7b666f7bf90718097caab03d94dbfdf724553
delernesto <delernesto832@gmail.com>
2025-08-20 23:51:31 +0300

    Add project files.

---
+++ DoctorAppointmentDemo.Data/Repositories/AppointmentRepository.cs
@@ -0,0 +1,34 @@
+﻿using MyDoctorAppointment.Data.Configuration;
+using MyDoctorAppointment.Data.Interfaces;
+using MyDoctorAppointment.Domain.Entities;
+
+namespace MyDoctorAppointment.Data.Repositories
+{
+    public class AppointmentRepository : GenericRepository<Appointment>, IAppointmentRepository
+    {
+        public override string Path { get; set; }
+
+        public override int LastId { get; set; }
+
+        public AppointmentRepository()
+        {
+            dynamic result = ReadFromAppSettings();
+
+            Path = result.Database.Doctors.Path;
+            LastId = result.Database.Doctors.LastId;
+        }
+
+        public override void ShowInfo(Doctor doctor)
+        {
+            Console.WriteLine(); // implement view of all object fields
+        }
+
+        protected override void SaveLastId()
+        {
+            dynamic result = ReadFromAppSettings();
+            result.Database.Doctors.LastId = LastId;
+
+            File.WriteAllText(Constants.AppSettingsPath, result.ToString());
+        }
+    }
+}
